LOSModified/android_frameworks_native

inputflinger: Increase SW_MAX *= 4
We increase the size of SW_MAX for two reasons:
1. SW_MACHINE_COVER which is SW_MAX is ignored, guessing an off by one
   somwehere
2. Samsung extends SW_ beyond standard SW_MAX. SW_FLIP goes to 0x15,
   standard SW_MAX 0x10

services/inputflinger/reader/include/EventHub.h
@@ -604,8 +604,8 @@ private:
         BitArray<KEY_MAX> keyState;
         BitArray<ABS_MAX> absBitmask;
         BitArray<REL_MAX> relBitmask;
-        BitArray<SW_MAX> swBitmask;
-        BitArray<SW_MAX> swState;
+        BitArray<SW_MAX * 4> swBitmask;
+        BitArray<SW_MAX * 4> swState;
         BitArray<LED_MAX> ledBitmask;
         BitArray<FF_MAX> ffBitmask;
         BitArray<INPUT_PROP_MAX> propBitmask;

alternoegraha/lineage_frameworks_native

services/inputflinger/reader/include/EventHub.h
@@ -656,8 +656,8 @@ private:
         BitArray<KEY_CNT> keyBitmask;
         BitArray<KEY_CNT> keyState;
         BitArray<REL_CNT> relBitmask;
-        BitArray<SW_CNT> swBitmask;
-        BitArray<SW_CNT> swState;
+        BitArray<SW_CNT * 4> swBitmask;
+        BitArray<SW_CNT * 4> swState;
         BitArray<LED_CNT> ledBitmask;
         BitArray<FF_CNT> ffBitmask;
         BitArray<INPUT_PROP_CNT> propBitmask;